aa25f78e99e2eefa5b748c7d60bd9d11d5856cfc
Samuel Quintano <samuelmalagonero21@gmail.com>
2025-11-13 11:31:10 +0100

    Activity detallesLibro

--- app/src/main/java/es/maestre/booktrack/DetallesLibro.kt
+++ app/src/main/java/es/maestre/booktrack/DetallesLibro.kt
@@ -6,19 +6,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import es.maestre.booktrack.databinding.ActivityDetallesLibroBinding
 import es.maestre.booktrack.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class DetallesLibro : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding : ActivityDetallesLibroBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityDetallesLibroBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
