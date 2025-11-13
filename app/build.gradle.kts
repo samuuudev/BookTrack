7bb06989d5f44d554ae01e98e04933d12be9a50b
Samuel Quintano <samuelmalagonero21@gmail.com>
2025-11-13 12:46:40 +0100

    Add Room and Fragment dependencies to build config
    Added Room database and Fragment KTX dependencies to app/build.gradle.kts. Enabled kapt plugin for annotation processing required by Room.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("org.jetbrains.kotlin.kapt")
 }
 
 android {
@@ -51,5 +52,11 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    
+
+    implementation("androidx.room:room-common:2.8.3")
+    implementation("androidx.room:room-runtime:2.8.3")
+    kapt("androidx.room:room-compiler:2.8.3")
+    implementation("androidx.room:room-ktx:2.8.3")
+    implementation("androidx.fragment:fragment-ktx:1.8.9")
+
 }
